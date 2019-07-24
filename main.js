a29aa617b72c1539b7a26bcbbab5b43594c8f504
etstringy <eddyteddyt@gmail.com>
2019-07-24 18:05:34 +0100

    v1.1
    FC Support, Abbreviaton support and some build stuff

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, ipcMain: ipc} = require('electron')
+const {app, BrowserWindow, ipcMain: ipc, ipcRenderer} = require('electron')
+const { autoUpdater } = require("electron-updater")
 const path = require('path')
 const settings = require('electron-settings');
 const client = require('discord-rich-presence')('602401007590309898');
@@ -29,7 +30,7 @@ function createWindow () {
   mainWindow.setMenu(null);
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -47,7 +48,13 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow()
+  if(settings.get("sp.fc") == "") {
+    settings.set('sp.fc', 'FC-0000-0000-0000');
+  }
+  autoUpdater.checkForUpdatesAndNotify()
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -69,15 +76,20 @@ app.on('activate', function () {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
+const startTimestamp = new Date();
+
 function setActivity() {
+    if(settings.get("sp.fc") == "") {
+      settings.set('sp.fc', 'Friend Code Hidden');
+    }
     client.updatePresence({
-      // state: 'Playing Nintendo Switch',
+      state: settings.get("sp.fc"),
       details: `Playing ${settings.get('sp.game')}`,
       largeImageKey: 'ns',
-      smallImageKey: settings.get('sp.img')
+      smallImageKey: settings.get('sp.img'),
+      startTimestamp
     });
 }
-
 
 setActivity();
 
@@ -90,15 +102,25 @@ ipc.on('updateGame', function (event, arg) {
   // console.log(`Debug> Presence set to ${arg}`)
   if(arg == "Super Smash Bros Ultimate") {
     settings.set('sp.img', 'smash');
+  } else if(arg == "SSBU") {
+    settings.set('sp.img', 'smash');
+  } else if(arg == "Smash Ultimate") {
+    settings.set('sp.img', 'smash');
   } else if(arg == "Fortnite") {
     settings.set('sp.img', 'fn');
   } else if(arg == "Minecraft") {
     settings.set('sp.img', 'mc');
+  } else if(arg == "MC") {
+    settings.set('sp.img', 'mc');
   } else if(arg == "Super Mario Maker 2") {
+    settings.set('sp.img', 'smm2');
+  } else if(arg == "SMM2") {
     settings.set('sp.img', 'smm2');
   } else if(arg == "Super Mario Party") {
     settings.set('sp.img', 'smp');
   } else if(arg == "Splatoon 2") {
+    settings.set('sp.img', 'splatoon_2');
+  } else if(arg == "Spla2n") {
     settings.set('sp.img', 'splatoon_2');
   } else if(arg == "Super Mario Odyssey") {
     settings.set('sp.img', 'super_mario_odyssey');
@@ -108,11 +130,31 @@ ipc.on('updateGame', function (event, arg) {
     settings.set('sp.img', 'undertale');
   } else if(arg == "YouTube") {
     settings.set('sp.img', 'yt');
+  } else if(arg == "YT") {
+    settings.set('sp.img', 'yt');
+  } else if(arg == "Deltarune") {
+    settings.set('sp.img', 'deltarune');
+  } else if(arg == "DELTARUNE") {
+    settings.set('sp.img', 'deltarune');
   } else {
     settings.set('sp.img', 'null');
   }
 
   settings.set('sp.game', arg);
+
+  if(arg == "") {
+    settings.set('sp.game', 'Nothing');
+  }
+
+  const startTimestamp = new Date();
 })
 
 ipc.on('destroyPresence', () => client.disconnect());
+
+ipc.on('fcmanage', (event, arg) => {
+  settings.set('sp.fc', arg);
+})
+
+ipc.on('reload', () => mainWindow.reload())
+
+ipc.on('friendcode', (event) => event.reply('friendcode-client', settings.get("sp.fc")))
--- renderer.js
+++ renderer.js
@@ -1,14 +1,41 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-var {ipcRenderer: ipc, remote} = require('electron')
+var {ipcRenderer: ipc, remote, shell} = require('electron')
+var $ = require('jQuery')
+
+document.getElementById("reconnect").disabled = true;
+document.getElementById("disconnect").disabled = false;
 
 function destroy() {
     ipc.send('destroyPresence')
+    M.toast({html: 'Disconnected from Discord', classes:'blue accent-2'})
+    document.getElementById("disconnect").disabled = true;
+    document.getElementById("reconnect").disabled = false;
 };
 
 function submit() {
     ipc.send('updateGame', document.getElementById("game").value)
+    ipc.send('fcmanage', document.getElementById("FC").value)
+    M.toast({html: 'Success!', classes:'green lighten-3'});
 }
 
-setInterval(() => console.log(document.getElementById("game").value), 15e3)
+function openGithub() {
+    event.preventDefault();
+    shell.openExternal("http://github.com/etstringy/switchpresence");
+}
+
+function reload() {
+    ipc.send('reload')
+}
+
+// setInterval(() => , 15e3)
+
+ipc.on('friendcode-client', function (event, arg) {
+    document.getElementById("FC").value = arg
+    if(document.getElementById("FC").value == "Friend Code Hidden") {
+        document.getElementById("FC").value = ""
+    }
+})
+
+ipc.send('friendcode')
